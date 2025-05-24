a41b56d56d64a11e66be45da816ca2f457a85130
Hitachi Hikari Church <128435515+hitachihikari@users.noreply.github.com>
2025-05-24 22:19:45 +0900

    Update and rename serviceWorker.js to service-worker.js

---
+++ message/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'hhc-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './hhc-icon-512.png',
+  './manifest.json',
+  './logo.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://code.jquery.com/jquery.js',
+  'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css',
+  'https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
